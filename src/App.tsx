7c0cac1023f0401add4b59851634599393c813cf
blackkat02 <boris02374@gmale.com>
2026-07-23 20:10:02 +0000

    feat: initial commit for demo standalone frontend

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import HomePage from "./page/HomePage";
+
+export default function App() {
+  const handleOpenSignIn = () => {
+    console.log("Open Sign In Modal");
+  };
+
+  const handleOpenSignUp = () => {
+    console.log("Open Sign Up Modal");
+  };
+
+  return (
+    <HomePage
+      isAuthenticated={false}
+      userBalance={0}
+      onOpenSignIn={handleOpenSignIn}
+      onOpenSignUp={handleOpenSignUp}
+    />
+  );
+}
